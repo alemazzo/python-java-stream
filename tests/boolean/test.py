8033ac4cd4bc43fe6dfe64f3d11afd1e0e373b6d
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-03-06 11:52:27 +0100

    Refactoring Structure

--- tests/boolean/test.py
+++ tests/boolean/test.py
@@ -4,6 +4,11 @@ from stream import BooleanStream
 
 class TestBooleanStream(unittest.TestCase):
 
+    def test_random(self):
+        s = BooleanStream.random().limit(20)
+        for elem in s:
+            self.assertIn(elem, [True, False])
+
     def test_true(self):
         s = BooleanStream.true().limit(10)
         for elem in s:
@@ -13,3 +18,8 @@ class TestBooleanStream(unittest.TestCase):
         s = BooleanStream.false().limit(10)
         for elem in s:
             self.assertFalse(elem)
+
+    def test_filter(self):
+        s = BooleanStream.random().limit(20).filter(lambda x: x)
+        for elem in s:
+            self.assertTrue(elem)
