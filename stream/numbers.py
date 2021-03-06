805075667aa232ee6cc7c1e34cadb79baa36ee94
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-03-06 18:15:42 +0100

    Added pi digits stream

--- stream/numbers.py
+++ stream/numbers.py
@@ -81,6 +81,23 @@ class NumberStream(Stream):
     def range(start, end, step=1):
         return NumberStream(Stream.iterate(start, lambda i: i + step).takeWhile(lambda x: x <= end))
 
+    @staticmethod
+    def pi():
+
+        def pi_digits():
+            "Generate n digits of Pi."
+            k, a, b, a1, b1 = 2, 4, 1, 12, 4
+            while True:
+                p, q, k = k * k, 2 * k + 1, k + 1
+                a, b, a1, b1 = a1, b1, p * a + q * a1, p * b + q * b1
+                d, d1 = a / b, a1 / b1
+                while d == d1:
+                    yield int(d)
+                    a, a1 = 10 * (a % b), 10 * (a1 % b1)
+                    d, d1 = a / b, a1 / b1
+
+        return NumberStream(Stream(pi_digits()))
+
     def __init__(self, _stream):
         if isinstance(_stream, Stream):
             self.iterable = _stream.iterable
